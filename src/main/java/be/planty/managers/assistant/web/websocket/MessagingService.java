98c94ad984515d319c58cb016b3ffb3c2af36e9e
Hadi Ahmadi <MacPersia@gmail.com>
2018-09-01 19:59:32 +0800

    Trying to use object as the payload type... (WIP)

--- src/main/java/be/planty/managers/assistant/web/websocket/MessagingService.java
+++ src/main/java/be/planty/managers/assistant/web/websocket/MessagingService.java
@@ -8,13 +8,13 @@ import be.planty.managers.assistant.repository.UserRepository;
 import be.planty.managers.assistant.security.SecurityUtils;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.messaging.Message;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
-import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Controller;
 
 import java.util.Optional;
@@ -23,6 +23,9 @@ import java.util.Optional;
 public class MessagingService {
 
     private static final Logger logger = LoggerFactory.getLogger(MessagingService.class);
+
+    private static final ObjectWriter objectWriter = new ObjectMapper().writerWithDefaultPrettyPrinter();
+    public static final String HEADER_KEY_SIMP_SESSION_ID = "simpSessionId";
 
     private final SimpMessageSendingOperations messagingTemplate;
 
@@ -41,23 +44,39 @@ public class MessagingService {
 
     @MessageMapping("/topic/action-requests/{emailAddress}")
     //@SendTo("/topic/action.req")
-    public void onRequest(@Payload Object requestPayload, @DestinationVariable String emailAddress,
-                          StompHeaderAccessor headerAccessor/*, Principal principal*/) {
-        logger.debug("On requestPayload from '" + headerAccessor.getSessionId() + "' for '" + emailAddress + "' : " + requestPayload);
+    public void onRequest(//@Payload final ActionRequest requestPayload,
+                          //final StompHeaderAccessor stompHeaderAccessor, final Principal principal
+                          final Message message, @DestinationVariable final String emailAddress) {
+
+        final Object requestPayload = message.getPayload();
+        //final String skillSessionId = stompHeaderAccessor.getSessionId();
+        final String skillSessionId = message.getHeaders().get(HEADER_KEY_SIMP_SESSION_ID, String.class);
+        logger.debug("On requestPayload from '" + skillSessionId + "' for '" + emailAddress + "' : " + requestPayload);
         final Optional<Agent> agent = agentRepo.findByEmailAddress(emailAddress).stream().findFirst();
         final String dest = "/queue/action-requests";
+
         final Optional<String> username = agent.map(a -> a.getUser().getLogin());
         final String prettyRequest = toPrettyString(requestPayload);
         logger.debug("Forwarding requestPayload to '" + username.orElse(null) + "' : '" + dest + "' : " + prettyRequest);
         assert username.isPresent();
-        this.messagingTemplate.convertAndSendToUser(username.get(), dest, requestPayload, headerAccessor.getMessageHeaders());
+
+        //this.messagingTemplate.convertAndSendToUser(username.get(), dest, requestPayload, stompHeaderAccessor.getMessageHeaders());
+        this.messagingTemplate.send("/user/" + encodeUsername(username) + dest, message);
+    }
+
+    private String encodeUsername(Optional<String> username) {
+        return username.get().replaceAll("/", "%2F");
     }
 
     @MessageMapping("/topic/action-responses")
     //@SendTo("/topic/action.res")
-    public void onResponse(@Payload Object responsePayload,
-                           StompHeaderAccessor headerAccessor/*, Principal principal*/) {
-        final String agentSessionId = headerAccessor.getSessionId();
+    public void onResponse(//@Payload final ActionResponse responsePayload,
+                           //final StompHeaderAccessor headerAccessor, /*final Principal principal*/
+                           final Message message) {
+
+        final Object responsePayload = message.getPayload();
+        //final String agentSessionId = headerAccessor.getSessionId();
+        final String agentSessionId = message.getHeaders().get(HEADER_KEY_SIMP_SESSION_ID, String.class);
         logger.debug("On responsePayload from '" + agentSessionId + "' : " + responsePayload);
         final Optional<String> agentUsername = SecurityUtils.getCurrentUserLogin();
         final Optional<String> emailAddress = agentUsername.flatMap(userRepo::findOneByLogin).map(User::getEmail);
@@ -67,7 +86,9 @@ public class MessagingService {
         final String prettyResponse = toPrettyString(responsePayload);
         logger.debug("Forwarding responsePayload to '" + username.orElse(null) + "' : '" + dest + "' : " + prettyResponse);
         assert username.isPresent();
-        this.messagingTemplate.convertAndSendToUser(username.get(), dest, responsePayload, headerAccessor.getMessageHeaders());
+
+        //this.messagingTemplate.convertAndSendToUser(username.get(), dest, responsePayload, headerAccessor.getMessageHeaders());
+        this.messagingTemplate.send("/user/" + encodeUsername(username) + dest, message);
     }
 
     private String toPrettyString(Object payload) {
@@ -75,12 +96,14 @@ public class MessagingService {
         try {
             prettyPayload = payload instanceof String ?
                 (String) payload
-                : new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(payload);
+                : (payload instanceof byte[] ?
+                    new String((byte[]) payload)
+                    : objectWriter.writeValueAsString(payload));
+
         } catch (JsonProcessingException e) {
             logger.error(e.getMessage(), e);
             prettyPayload = String.valueOf(payload);
         }
         return prettyPayload;
     }
-
 }
