cd761edd80f4124ee67c6085a0cf483b0957c10a
Hadi <macpersia@gmail.com>
2019-04-28 17:17:32 +0800

    using jib's entrypoint

--- src/main/docker/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Added by Hadi
-if [ -z "$JWT_SECRET_KEY" ]; then JWT_SECRET_KEY=$(openssl rand -base64 64 | tr '\n' -d) ; fi
-JAVA_OPTS="${JAVA_OPTS} -Djhipster.security.authentication.jwt.secret=${JWT_SECRET_KEY}"
-#echo "The secret JWT key is ${JWT_SECRET_KEY:?absent}."
-
-echo "The application will start in ${JHIPSTER_SLEEP}s..." && sleep ${JHIPSTER_SLEEP}
-exec java ${JAVA_OPTS} -Djava.security.egd=file:/dev/./urandom -jar "${HOME}/app.war" "$@"
